ad06a3d79883e39f4af7f5b25d6541871d5d82fa
Kássio Silva <kassio.slva@gmail.com>
2024-11-28 20:22:13 -0300

    feat: add register donor integration

--- src/pages/app/register-donor.jsx
+++ src/pages/app/register-donor.jsx
@@ -1,17 +1,61 @@
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
+import { toast } from 'sonner'
+
+import { registerDonor } from '../../api/register-donor'
 import styles from './register-donor.module.css'
 
 export function RegisterDonor() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+
+  const { mutateAsync: createDonor, isPending } = useMutation({
+    mutationFn: registerDonor,
+  })
+
+  function handleName(e) {
+    setName(e.target.value)
+  }
+
+  function handleEmail(e) {
+    setEmail(e.target.value)
+  }
+
+  function handlePhone(e) {
+    setPhone(e.target.value)
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault()
+
+    try {
+      await createDonor({ name, email, phone })
+
+      toast.success('Doador cadastrado!')
+    } catch (error) {
+      toast.error('Não foi possível cadastrar doador.')
+    }
+  }
+
   return (
     <div className={`${styles.main} vh-100`}>
       <header className={styles.header}>
         <h1>PÁGINA DE CADASTRO</h1>
       </header>
 
-      <form>
+      <form onSubmit={onSubmit} className="needs-validation" autoComplete="off">
         <div className="row">
           <div className="col">
             <div className={styles.inputContainer}>
-              <input type="text" id="nome" name="nome" placeholder="NOME" />
+              <input
+                type="text"
+                placeholder="NOME"
+                value={name}
+                onChange={handleName}
+                required
+                autoFocus
+              />
             </div>
           </div>
 
@@ -19,9 +63,11 @@ export function RegisterDonor() {
             <div className={styles.inputContainer}>
               <input
                 type="email"
-                id="email"
-                name="email"
                 placeholder="E-MAIL"
+                value={email}
+                onChange={handleEmail}
+                required
+                autoFocus
               />
             </div>
           </div>
@@ -32,17 +78,36 @@ export function RegisterDonor() {
             <div className={styles.inputContainer}>
               <input
                 type="tel"
-                id="telefone"
-                name="telefone"
                 placeholder="TELEFONE"
+                value={phone}
+                onChange={handlePhone}
+                required
+                autoFocus
+                pattern="(\d{3}\d{3}\d{3}\d{2})"
+                maxLength="11"
               />
             </div>
           </div>
 
           <div className="col">
             <div className={styles.inputContainer}>
-              <button type="button" className="btn btn-primary">
-                CADASTRE-SE
+              <button
+                type="submit"
+                className="btn btn-primary"
+                disabled={isPending}
+              >
+                {isPending ? (
+                  <>
+                    <span
+                      className="spinner-border spinner-border-sm"
+                      aria-hidden="true"
+                    />
+
+                    <span role="status">Cadastrando ...</span>
+                  </>
+                ) : (
+                  'CADASTRE-SE'
+                )}
               </button>
             </div>
           </div>
